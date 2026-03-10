0275cceddd1050adc6c75a18bde888c9a959e10c
heleledd <157613131+heleledd@users.noreply.github.com>
2026-03-10 14:36:55 +0000

    change so that inputs can be changed in the gh actions run

--- src/config.py
+++ src/config.py
@@ -4,8 +4,11 @@ TARGET_CRS = 'EPSG:27700'
 
 # Cardiff bounding box (west, south, east, north) in WGS84
 # CARDIFF_BBOX = (-3.35, 51.37, -3.05, 51.57) # coordinates for all of Cardiff
+# CARDIFF_BBOX = (-3.196, 51.494, -3.180, 51.500) # smaller box for testing
 
-CARDIFF_BBOX = (-3.196, 51.494, -3.180, 51.500) # smaller box for testing
+
+bbox_string = os.environ.get('BBOX', '-3.196, 51.494, -3.180, 51.500')
+CARDIFF_BBOX = tuple(float(coord.strip()) for coord in bbox_string.split(','))
 
 GRID_SIZE_METERS = 100
 
@@ -14,5 +17,8 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_DATA_DIR = os.path.join(SCRIPT_DIR, '../data/input_data')
 OUTPUT_DATA_DIR = os.path.join(SCRIPT_DIR, '../data/output')
 
-PARK_ACCESS_POINTS_PATH = os.path.join(INPUT_DATA_DIR, 'cardiff_greenspace_access_points.geojson')
-PARK_BOUNDARY_PATH = os.path.join(INPUT_DATA_DIR, 'cardiff_greenspace_sites.geojson')
+access_points_filename = os.environ.get('ACCESS_POINTS_FILE', 'cardiff_greenspace_access_points.geojson')
+boundary_filename = os.environ.get('BOUNDARY_FILE', 'cardiff_greenspace_sites.geojson')
+
+PARK_ACCESS_POINTS_PATH = os.path.join(INPUT_DATA_DIR, access_points_filename)
+PARK_BOUNDARY_PATH = os.path.join(INPUT_DATA_DIR, boundary_filename)
